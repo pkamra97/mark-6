cb01dc8244f77583ffdc5d71debf07b97ff13bf1
pkamra97 <109149360+pkamra97@users.noreply.github.com>
2022-10-02 20:57:14 +0530

    feat : translation

--- file.js
+++ file.js
@@ -2,6 +2,40 @@ var input=document.querySelector("#ta1");
 var outpur=document.querySelector("#ta2");
 var translate=document.querySelector("#btn");
 const opDiv=document.querySelector("#opd");
+var url="https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text)
+{
+    return url +"?text="+text;
+}
+function errorhandler(error)
+{
+    console.log(error);
+    alert("somewthing went wrong try again after some time");
+}
+
+function minion()
+{
+    fetch(getTranslationUrl(input.value))
+    .then(response => response.json())
+    // .then(json => console.log(json.contents.translated))
+    .then(json=> {
+        var translated_Text=json.contents.translated;
+        ta2.innerText=translated_Text;
+    })
+    .catch(errorhandler);
+}
+translate.addEventListener('click',minion);
+
+
+
+
+
+
+
+
+
+
 
 // ex04
 // console.log(input);
@@ -28,11 +62,21 @@ const opDiv=document.querySelector("#opd");
 
 //doubt-1 why it works even without clicking and why the click is not working in other browser
 
+// calling a mock server and getting a response
+//aslo writing test cases in case of failure
 
-
-function minion()
-{
-    ta2.innerText=   `${input.value} translated into = Minion language`;
-}
-
-translate.addEventListener("click", minion);
+// function checkerror(error)
+// {
+//     console.log(error);
+//     // console.log("server failed to load");
+//     alert("something went wrong with the server");
+// }
+// function lec4testapi()
+// {
+//     ta2.innerText=   `${input.value} translated into = Minion language`;
+//     var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=i am iron man";
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(json => console.log(json.contents.translated))
+//     .catch(checkerror);
+// }
